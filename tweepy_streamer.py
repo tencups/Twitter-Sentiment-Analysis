4ade4577baabfbac52b0131ab5ffd222609ede7d
tencups <26286782+tencups@users.noreply.github.com>
2018-12-21 22:00:41 -0500

    add files

---
+++ tweepy_streamer.py
@@ -0,0 +1,45 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import twitter_credentials
+
+class TwitterStreamer():
+    """
+    Class for Streaming and processing live tweets
+    """
+    def stream_tweet(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter Authentication and the connection to the Twitter Streaming API
+        listener = StdOutListener()
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+
+        stream = Stream(auth, listener)
+
+        stream.filter(track=hash_tag_list)
+
+class StdOutListener(StreamListener):
+    """
+    This is a basic listener class that just prints recieved tweets to stdout
+    """
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
+
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename,'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+if __name__ == "__main__":
+    hash_tag_list = ['donald trump','barack obama','bernie sanders','hillary clinton']
+    fetched_tweets_filename = 'tweets.json'
+
+    twitter_stremear = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename,hash_tag_list)
