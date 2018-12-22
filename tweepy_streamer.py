32c6c363e5c9dde0ef25be965683c258522bb9de
tencups <26286782+tencups@users.noreply.github.com>
2018-12-21 22:06:09 -0500

    remove spaces

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -12,9 +12,7 @@ class TwitterStreamer():
         listener = StdOutListener()
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
         stream = Stream(auth, listener)
-
         stream.filter(track=hash_tag_list)
 
 class StdOutListener(StreamListener):
