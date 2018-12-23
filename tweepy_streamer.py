151276562019b7c79240b9d3ba0ad7047b686f78
tencups <26286782+tencups@users.noreply.github.com>
2018-12-23 13:49:37 -0500

    add analysis

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,21 +1,64 @@
 from tweepy.streaming import StreamListener
+from tweepy import API
+from tweepy import Cursor
 from tweepy import OAuthHandler
 from tweepy import Stream
+
 import twitter_credentials
+import numpy as np
+import pandas as pd
+
+class TwitterClient():
+    def __init__(self, twitter_user = None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client = API(self.auth)
+        self.twitter_user = twitter_user
+
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline,id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+
+    def get_friend_list(self,num_friends):
+        friend_list = []
+        for friend in Cursor(self.twitter_client.friends, id=self.twitter_user).items(num_friends):
+            friends.append(friend)
+        return friend_list
+
+    def get_home_timeline_tweets(self,num_tweets):
+        home_timeline_tweets = []
+        for tweet in Cursor(self.twitter_client.home_timeline, id=self.twitter_user).items(num_tweets):
+            home_timeline_tweets.append(tweet)
+        return home_timeline_tweets
+
+
+class TwitterAuthenticator():
+
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        return auth
 
 class TwitterStreamer():
     """
     Class for Streaming and processing live tweets
     """
+    def __init__(self):
+        self.twitter_authenticator = TwitterAuthenticator()
+
     def stream_tweet(self, fetched_tweets_filename, hash_tag_list):
         # This handles Twitter Authentication and the connection to the Twitter Streaming API
-        listener = StdOutListener()
-        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        listener = TwitterListener(fetched_tweets_filename)
+        auth = self.twitter_authenticator.authenticate_twitter_app()
         stream = Stream(auth, listener)
+        # This line filters Twitter Streams by capturing the data by the keywords
         stream.filter(track=hash_tag_list)
 
-class StdOutListener(StreamListener):
+class TwitterListener(StreamListener):
     """
     This is a basic listener class that just prints recieved tweets to stdout
     """
@@ -33,11 +76,40 @@ class StdOutListener(StreamListener):
         return True
 
     def on_error(self, status):
+        if status == 420:
+            # Returning false on_data method in case rate limit occurs
+            return False
         print(status)
+class TweetAnalyzer():
+    """
+    Functionality for analyzing and categorizing content from tweetself.
+    """
+    def tweets_to_data_frame(self,tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=["Tweets"])
+        df['id'] = np.array([tweet.id for tweet in tweets])
+        df['length'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets']= np.array([tweet.retweet_count for tweet in tweets])
 
+        return df
 if __name__ == "__main__":
-    hash_tag_list = ['donald trump','barack obama','bernie sanders','hillary clinton']
-    fetched_tweets_filename = 'tweets.json'
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
+    api = twitter_client.get_twitter_client_api()
 
-    twitter_stremear = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_filename,hash_tag_list)
+    tweets = api.user_timeline(screen_name="realDonaldTrump",count=20)
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    print(df.head(10))
+    #print(tweets[0].retweet_count)
+    #print(dir(tweets[0]))
+
+
+    #hash_tag_list = ['donald trump','barack obama','bernie sanders','hillary clinton']
+    #fetched_tweets_filename = 'tweets.json'
+    #twitter_client = TwitterClient('pycon')
+    #print(twitter_client.get_user_timeline_tweets(1))
+
+    #twitter_streamer = TwitterStreamer()
+    #twitter_streamer.stream_tweet(fetched_tweets_filename,hash_tag_list)
